f3cd48bfee23bc2cd4451b14b8c4240df81bc727
thkim <thkim>
2018-09-28 08:12:06 +0000

    - ¼öÁ¤

---
+++ src/kr/co/rtst/autosar/ap4x/core/util/AdaptiveAutosar4xReleaseDescriptor.java
@@ -0,0 +1,13 @@
+package kr.co.rtst.autosar.ap4x.core.util;
+
+import autosar40.util.Autosar40ReleaseDescriptor;
+
+public class AdaptiveAutosar4xReleaseDescriptor extends Autosar40ReleaseDescriptor {
+	
+	@Override
+	public String getDefaultContentTypeId() {
+		// TODO Auto-generated method stub
+		return "kr.co.rtst.autosar.ap4x.core.contenttype.aaswcFile";
+	}
+
+}
